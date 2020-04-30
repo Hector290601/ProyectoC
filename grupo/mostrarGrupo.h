6ac0902226a63e61253b7c23d6051cddd6d969c5
Hector290601 <33700655+Hector290601@users.noreply.github.com>
2020-04-30 16:49:32 -0500

    Update mostrarGrupo.h

--- grupo/mostrarGrupo.h
+++ grupo/mostrarGrupo.h
@@ -2,12 +2,14 @@ void mostrarGrupo(Grupo *h, int id){
 	Grupo* aux = h;
 	printf("antes: %p\n", h);
 	while(aux->id != id){
-		aux = aux->sig;
-		printf("%p -> ", aux);
 		if(aux->id = id){
 			break;
+		}else{
+			aux = aux->sig;
+			printf("%p -> ", aux);
 		}
 	}
 	printf("al finalh: %p\n", aux);
 	printf("ID: %d\nNúmero de Alumnos: %d\nProfesor: %s\n", aux->id, aux->numeroDeAlumnos, aux->maestro);
+	free(aux);
 }
